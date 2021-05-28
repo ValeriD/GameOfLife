cd9832702efe011f391a70307ae0f0090a151bbe
ValeriD1 <valeri.dobrev10@gmail.com>
2021-05-28 23:03:36 +0300

    Added doc and changed some variable names

--- scripts/cell.js
+++ scripts/cell.js
@@ -13,8 +13,46 @@ class Cell{
     }
 
 
-    determineNextGenOfCell(x, y, maxX, maxY) {
-        var numberOfAliveNeighbours = getNumberOfAliveNeighbours(x, y, maxX, maxY);
+    /**
+     * Method that gets the count of the alive neighbors.
+     * @param {*} row current row that the cell is on the board
+     * @param {*} col current column the cell is on the board
+     * @param {*} rows maximum number of rows
+     * @param {*} cols maximum number of columns
+     * @returns the number of alive neighbors
+     */
+    getNumberOfAliveNeighbours(row, col, rows, cols){
+        var numberOfAliveNeighbours = 0;
+        //Check for valid indexes
+        if(row<0 || col<0) return -1;
+
+        //Check by row
+        if(row!=0 && this.board[row-1][col].isAlive) numberOfAliveNeighbours++;
+        if(row+1 < rows && this.board[row+1][col].isAlive) numberOfAliveNeighbours++;
+        
+        //Check by column
+        if(col!=0 && this.board[col-1][col].isAlive) numberOfAliveNeighbours++;
+        if(col+1 < cols && this.board[row][col+1].isAlive) numberOfAliveNeighbours++;
+
+        //Check diagonals
+        if(row>0){
+            if((col>0 && this.board[row-1][col-1].isAlive) || (col< cols-1 && board[row][col+1].isAlive)) numberOfAliveNeighbours++;
+        }
+        if(row<rows-1){
+            if(( col>0 && board[row][col+1].isAlive) || (col< cols-1 && board[row+1][col+1])) numberOfAliveNeighbours++;
+        }
+
+        return numberOfAliveNeighbours;
+    }
+    /**
+     * Method that determines if the next generation of the cell will be alive or not
+     * @param {*} row on which the cell is on the board
+     * @param {*} col on which the cell is on the board
+     * @param {*} rows maximum number of rows on the board
+     * @param {*} cols maximum number of columns on the board
+     */
+    determineNextGenOfCell(row, col, rows, cols) {
+        var numberOfAliveNeighbours = getNumberOfAliveNeighbours(row, col, rows, cols);
         
         //Living cell dies if more than 3 neighbors are alive (Overpopulation)
         //Living cell with less than two alive nejghbours dies (Loneliness)
@@ -22,34 +60,11 @@ class Cell{
             if(numberOfAliveNeighbours > 3 || numberOfAliveNeighbours < 2){
                 this.nextGen = false;
             }
-        }else if(numberOfAliveNeighbours == 3) this.nextGen = true;
+        }else if(numberOfAliveNeighbours == 3) this.nextGen = true; //If the number of alive neighbors is exactly 3, then the cell will became alive
         else{
+            //Stall 
             this.nextGen = this.isAlive;
         }
-    }
-
-    getNumberOfAliveNeighbours(x, y, maxX, maxY){
-        var numberOfAliveNeighbours = 0;
-        //Check for valid indexes
-        if(x<0 || y<0) return -1;
-
-        //Check by X
-        if(x!=0 && this.board[x-1][y].isAlive) numberOfAliveNeighbours++;
-        if(x+1 < maxX && this.board[x+1][y].isAlive) numberOfAliveNeighbours++;
-        
-        //Check by Y
-        if(y!=0 && this.board[y-1][y].isAlive) numberOfAliveNeighbours++;
-        if(y+1 < maxY && this.board[x][y+1].isAlive) numberOfAliveNeighbours++;
-
-        //Check diagonals
-        if(x>0){
-            if((y>0 && this.board[x-1][y-1].isAlive) || (y< maxY-1 && board[x][y+1].isAlive)) numberOfAliveNeighbours++;
-        }
-        if(x<maxX-1){
-            if(( y>0 && board[x][y+1].isAlive) || (y< maxY-1 && board[x+1][y+1])) numberOfAliveNeighbours++;
-        }
-
-        return numberOfAliveNeighbours;
     }
 
     getNextGenOfCell(){ 
@@ -64,6 +79,12 @@ class Cell{
     setWidth(width) { this.width = width; }
     setHeight(height) { this.height = height; }
 
+    /**
+     * Method that draws the cell to the given context
+     * @param  context 
+     * @param  xpos, the X position on which the cell should be drawn
+     * @param  ypos the Y position on which the cell should be drawn 
+     */
     draw(context, xpos, ypos){
         this.isAlive? context.fillStyle = "#000000" : context.fillStyle = "#FFFFFF";
         context.rect(xpos, ypos, this.width, this.height);
